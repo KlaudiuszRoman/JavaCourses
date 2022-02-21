f8d3464db9ca0242cb45b36da2a854282b90f123
Klaudiusz Roman <klaudiuszr@gmail.com>
2022-02-21 15:30:18 +0100

    Introduction to Java - data types

--- 001/java001/src/com/example/basics/Main.java
+++ 001/java001/src/com/example/basics/Main.java
@@ -3,6 +3,7 @@ package com.example.basics;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Hello World!");
+        System.out.println("Hello Again!");
     }
 }
---
+++ 001/java001/src/com/example/basics/data/types/J03_dloatDouble.java
@@ -0,0 +1,11 @@
+package com.example.basics.data.types;
+
+public class J03_dloatDouble {
+    public static void main(String[] args) {
+        float number = 10.5f;
+        double bigNumber = 23.234234234d;
+
+        System.out.println("Number: " + number
+                + " bigNumber: " + bigNumber);
+    }
+}
