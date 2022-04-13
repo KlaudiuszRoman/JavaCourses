c7a4fd082ad1c9de3ed0e513efe7fbb66b9e623a
KlaudiuszRoman <klaudiuszr@gmail.com>
2022-04-13 18:24:19 +0200

    Working with databases in Java

---
+++ 001/JDBC_mysql/src/main/java/org/example/JDBC_connection.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBC_connection {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/test",
+                    "root",
+                    ""
+            );
+            if(connection == null) {
+                System.out.println("Brak połączia z bazą danych");
+            } else {
+                System.out.println("Połączono!");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
