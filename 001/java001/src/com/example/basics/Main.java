8b9d1f4766da0cfddde27720523222ae1b929242
KlaudiuszRoman <klaudiuszr@gmail.com>
2022-05-18 15:25:09 +0200

    One to many relation in Hibernate

--- 001/java001/src/com/example/basics/Main.java
+++ 001/java001/src/com/example/basics/Main.java
@@ -1,10 +1,12 @@
 package com.example.basics;
 
 
+
 public class Main {
 
+
     public static void main(String[] args) {
-	    System.out.println("Hello World!");
-        System.out.println("Hello Again!");
+
+        System.out.println("hello world!");
     }
 }
